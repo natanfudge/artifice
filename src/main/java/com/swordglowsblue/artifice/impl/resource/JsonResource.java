18862a018f39f644b3eda55207eab21cd76f4b12
Connor Scialdone <cj@swordglowsblue.com>
2019-06-14 17:12:11 -0600

    Allow any JsonElement as root in JsonResource

--- src/main/java/com/swordglowsblue/artifice/impl/resource/JsonResource.java
+++ src/main/java/com/swordglowsblue/artifice/impl/resource/JsonResource.java
@@ -1,6 +1,6 @@
 package com.swordglowsblue.artifice.impl.resource;
 
-import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
 import com.swordglowsblue.artifice.api.ArtificeResource;
 import org.apache.commons.io.input.ReaderInputStream;
 
@@ -8,11 +8,11 @@ import java.io.InputStream;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 
-public class JsonResource implements ArtificeResource<JsonObject> {
-    private final JsonObject root;
-    public JsonResource(JsonObject root) { this.root = root; }
+public class JsonResource implements ArtificeResource<JsonElement> {
+    private final JsonElement root;
+    public JsonResource(JsonElement root) { this.root = root; }
 
-    public JsonObject getData() { return this.root; }
+    public JsonElement getData() { return this.root; }
     public InputStream toInputStream() {
         return new ReaderInputStream(new StringReader(this.getData().toString()), StandardCharsets.UTF_8);
     }
