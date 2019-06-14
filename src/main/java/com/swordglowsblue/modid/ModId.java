a834cebb214443a3c05f0655500b63b16fd24b64
Connor Scialdone <cj@swordglowsblue.com>
2019-06-14 03:47:58 +0000

    Initial commit

---
+++ src/main/java/com/swordglowsblue/modid/ModId.java
@@ -0,0 +1,9 @@
+package com.swordglowsblue.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModId implements ModInitializer {
+    public void onInitialize() {
+        System.err.println("SGB mod template initialized unchanged!");
+    }
+}
---
+++ src/main/java/com/swordglowsblue/modid/ModIdClient.java
@@ -0,0 +1,9 @@
+package com.swordglowsblue.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModIdClient implements ClientModInitializer {
+    public void onInitializeClient() {
+        System.err.println("SGB mod template client initialized unchanged!");
+    }
+}
