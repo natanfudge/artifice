5cd2388b25fe1df7ba77027197038c70729cfd33
CatCore <arthur.bambou@gmail.com>
2020-07-09 14:43:30 +0200

    Carvers and SurfaceBuilder

--- src/main/java/com/swordglowsblue/artifice/api/builder/data/biome/BiomeBuilder.java
+++ src/main/java/com/swordglowsblue/artifice/api/builder/data/biome/BiomeBuilder.java
@@ -1,14 +1,17 @@
 package com.swordglowsblue.artifice.api.builder.data.biome;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder;
 import com.swordglowsblue.artifice.api.resource.JsonResource;
 import com.swordglowsblue.artifice.api.util.Processor;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStep;
 
 public class BiomeBuilder extends TypedJsonBuilder<JsonResource<JsonObject>> {
     public BiomeBuilder() {
         super(new JsonObject(), JsonResource::new);
+        this.root.add("carvers", new JsonObject());
     }
 
     public BiomeBuilder depth(float depth) {
@@ -46,6 +49,11 @@ public class BiomeBuilder extends TypedJsonBuilder<JsonResource<JsonObject>> {
         return this;
     }
 
+    public BiomeBuilder surface_builder(String surface_builder) {
+        this.root.addProperty("surface_builder", surface_builder);
+        return this;
+    }
+
     public BiomeBuilder precipitation(Biome.Precipitation precipitation) {
         this.root.addProperty("precipitation", precipitation.getName());
         return this;
@@ -58,6 +66,24 @@ public class BiomeBuilder extends TypedJsonBuilder<JsonResource<JsonObject>> {
 
     public BiomeBuilder effects(Processor<BiomeEffectsBuilder> biomeEffectsBuilder) {
         with("effects", JsonObject::new, biomeEffects -> biomeEffectsBuilder.process(new BiomeEffectsBuilder()).buildTo(biomeEffects));
+        return this;
+    }
+
+    private BiomeBuilder addCarver(GenerationStep.Carver carver, String[] configuredCaverIDs) {
+        for (String configuredCaverID : configuredCaverIDs)
+            this.root.getAsJsonObject("carvers").getAsJsonArray(carver.getName()).add(configuredCaverID);
+        return this;
+    }
+
+    public BiomeBuilder addAirCarvers(String... configuredCarverIds) {
+        this.root.getAsJsonObject("carvers").add(GenerationStep.Carver.AIR.getName(), new JsonArray());
+        this.addCarver(GenerationStep.Carver.AIR, configuredCarverIds);
+        return this;
+    }
+
+    public BiomeBuilder addLiquidCarvers(String... configuredCarverIds) {
+        this.root.getAsJsonObject("carvers").add(GenerationStep.Carver.LIQUID.getName(), new JsonArray());
+        this.addCarver(GenerationStep.Carver.LIQUID, configuredCarverIds);
         return this;
     }
 }
