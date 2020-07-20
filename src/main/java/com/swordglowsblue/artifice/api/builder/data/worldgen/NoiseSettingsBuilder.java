aa1d1636b4fc3e762ac3b0fbf5d3eaa37382e89d
CatCore <arthur.bambou@gmail.com>
2020-07-20 15:39:38 +0200

    Add noise settings builder and fix dimension builder

--- src/main/java/com/swordglowsblue/artifice/api/builder/data/worldgen/NoiseSettingsBuilder.java
+++ src/main/java/com/swordglowsblue/artifice/api/builder/data/worldgen/NoiseSettingsBuilder.java
@@ -1,13 +1,16 @@
-package com.swordglowsblue.artifice.api.builder.data.dimension;
+package com.swordglowsblue.artifice.api.builder.data.worldgen;
 
 import com.google.gson.JsonObject;
 import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder;
 import com.swordglowsblue.artifice.api.builder.data.StateDataBuilder;
+import com.swordglowsblue.artifice.api.builder.data.dimension.NoiseConfigBuilder;
+import com.swordglowsblue.artifice.api.builder.data.dimension.StructureManagerBuilder;
+import com.swordglowsblue.artifice.api.resource.JsonResource;
 import com.swordglowsblue.artifice.api.util.Processor;
 
-public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
-    public GeneratorSettingsBuilder() {
-        super(new JsonObject(), j->j);
+public class NoiseSettingsBuilder extends TypedJsonBuilder<JsonResource<JsonObject>> {
+    public NoiseSettingsBuilder() {
+        super(new JsonObject(), JsonResource::new);
     }
 
     /**
@@ -15,7 +18,7 @@ public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
      * @param bedrockRoofPosition
      * @return
      */
-    public GeneratorSettingsBuilder bedrockRoofPosition(int bedrockRoofPosition) {
+    public NoiseSettingsBuilder bedrockRoofPosition(int bedrockRoofPosition) {
         if (bedrockRoofPosition > 276) throw new IllegalArgumentException("bedrockRoofPosition can't be higher than 276! Found " + bedrockRoofPosition);
         if (bedrockRoofPosition < -20) throw new IllegalArgumentException("bedrockRoofPosition can't be smaller than -20! Found " + bedrockRoofPosition);
         this.root.addProperty("bedrock_roof_position", bedrockRoofPosition);
@@ -27,7 +30,7 @@ public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
      * @param bedrockFloorPosition
      * @return
      */
-    public GeneratorSettingsBuilder bedrockFloorPosition(int bedrockFloorPosition) {
+    public NoiseSettingsBuilder bedrockFloorPosition(int bedrockFloorPosition) {
         if (bedrockFloorPosition > 276) throw new IllegalArgumentException("bedrockFloorPosition can't be higher than 276! Found " + bedrockFloorPosition);
         if (bedrockFloorPosition < -20) throw new IllegalArgumentException("bedrockFloorPosition can't be smaller than -20! Found " + bedrockFloorPosition);
         this.root.addProperty("bedrock_floor_position", bedrockFloorPosition);
@@ -39,7 +42,7 @@ public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
      * @param seaLevel
      * @return
      */
-    public GeneratorSettingsBuilder seaLevel(int seaLevel) {
+    public NoiseSettingsBuilder seaLevel(int seaLevel) {
         if (seaLevel > 255) throw new IllegalArgumentException("Sealevel can't be higher than 255! Found " + seaLevel);
         if (seaLevel < 0) throw new IllegalArgumentException("Sealevel can't be smaller than 0! Found " + seaLevel);
         this.root.addProperty("sea_level", seaLevel);
@@ -51,7 +54,7 @@ public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
      * @param noiseConfigBuilder
      * @return
      */
-    public GeneratorSettingsBuilder noiseConfig(Processor<NoiseConfigBuilder> noiseConfigBuilder) {
+    public NoiseSettingsBuilder noiseConfig(Processor<NoiseConfigBuilder> noiseConfigBuilder) {
         with("noise", JsonObject::new, jsonObject -> noiseConfigBuilder.process(new NoiseConfigBuilder()).buildTo(jsonObject));
         return this;
     }
@@ -60,7 +63,7 @@ public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
      * @param disableMobGeneration
      * @return
      */
-    public GeneratorSettingsBuilder disableMobGeneration(boolean disableMobGeneration) {
+    public NoiseSettingsBuilder disableMobGeneration(boolean disableMobGeneration) {
         this.root.addProperty("disable_mob_generation", disableMobGeneration);
         return this;
     }
@@ -71,7 +74,7 @@ public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
      * @param blockStateBuilderProcessor
      * @return
      */
-    public GeneratorSettingsBuilder setBlockState(String id, Processor<StateDataBuilder> blockStateBuilderProcessor) {
+    public NoiseSettingsBuilder setBlockState(String id, Processor<StateDataBuilder> blockStateBuilderProcessor) {
         with(id, JsonObject::new, jsonObject -> blockStateBuilderProcessor.process(new StateDataBuilder()).buildTo(jsonObject));
         return this;
     }
@@ -81,7 +84,7 @@ public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
      * @param blockStateBuilderProcessor
      * @return
      */
-    public GeneratorSettingsBuilder defaultBlock(Processor<StateDataBuilder> blockStateBuilderProcessor) {
+    public NoiseSettingsBuilder defaultBlock(Processor<StateDataBuilder> blockStateBuilderProcessor) {
         return this.setBlockState("default_block", blockStateBuilderProcessor);
     }
 
@@ -90,7 +93,7 @@ public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
      * @param blockStateBuilderProcessor
      * @return
      */
-    public GeneratorSettingsBuilder defaultFluid(Processor<StateDataBuilder> blockStateBuilderProcessor) {
+    public NoiseSettingsBuilder defaultFluid(Processor<StateDataBuilder> blockStateBuilderProcessor) {
         return this.setBlockState("default_fluid", blockStateBuilderProcessor);
     }
 
@@ -99,7 +102,7 @@ public class GeneratorSettingsBuilder extends TypedJsonBuilder<JsonObject> {
      * @param structureManagerBuilder
      * @return
      */
-    public GeneratorSettingsBuilder structureManager(Processor<StructureManagerBuilder> structureManagerBuilder) {
+    public NoiseSettingsBuilder structureManager(Processor<StructureManagerBuilder> structureManagerBuilder) {
         with("structures", JsonObject::new, jsonObject -> structureManagerBuilder.process(new StructureManagerBuilder()).buildTo(jsonObject));
         return this;
     }
