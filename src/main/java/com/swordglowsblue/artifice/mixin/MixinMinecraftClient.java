7857fc0d14f07d3e61ddea74e54194119d0d78d3
OliviaTheVampire <sindrefagerheim53@gmail.com>
2021-02-19 10:33:39 +0100

    updated to 21w07a (#29)

--- src/main/java/com/swordglowsblue/artifice/mixin/MixinMinecraftClient.java
+++ src/main/java/com/swordglowsblue/artifice/mixin/MixinMinecraftClient.java
@@ -1,34 +1,25 @@
 package com.swordglowsblue.artifice.mixin;
 
-import java.util.Collection;
-
 import com.swordglowsblue.artifice.api.virtualpack.ArtificeResourcePackContainer;
-import com.swordglowsblue.artifice.impl.ArtificeAssetsResourcePackProvider;
-import com.swordglowsblue.artifice.impl.ArtificeDataResourcePackProvider;
-import net.minecraft.resource.ResourcePackProfile;
-import org.apache.commons.lang3.ArrayUtils;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.resource.ResourcePackManager;
-import net.minecraft.resource.ResourcePackProvider;
-
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.resource.ResourcePackProfile;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Collection;
 
 @Environment(EnvType.CLIENT)
 @Mixin(MinecraftClient.class)
 public abstract class MixinMinecraftClient {
 
-//	@ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourcePackManager;<init>(Lnet/minecraft/resource/ResourcePackProfile$class_5351;[Lnet/minecraft/resource/ResourcePackProvider;)V"), index = 1)
-//	private ResourcePackProvider[] appendArtificeAssets(ResourcePackProvider[] vanillaProviders) {
-//		return ArrayUtils.add(vanillaProviders, new ArtificeAssetsResourcePackProvider());
-//	}
+	/*@ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourcePackManager;<init>(Lnet/minecraft/resource/ResourcePackProfile$class_5351;[Lnet/minecraft/resource/ResourcePackProvider;)V"), index = 1)
+	private ResourcePackProvider[] appendArtificeAssets(ResourcePackProvider[] vanillaProviders) {
+		return ArrayUtils.add(vanillaProviders, new ArtificeAssetsResourcePackProvider());
+	}*/
 
 	@Redirect(method = "<init>", at = @At(value = "INVOKE",
 					target = "Lnet/minecraft/resource/ResourcePackManager;scanPacks()V"))
